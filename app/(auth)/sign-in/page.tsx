79b6db4a1148ff57d02bb347f3cc0c572e5ad931
ZEALOT <phinehasdscience@gmail.com>
2025-03-13 23:22:46 +0000

    refactor the themes in the globals.css

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className='text-gray-25 text-2xl '>
+      <p className='text-success-600'>Sign-in </p>
+      <h2 className='text-bankGradient'>Welcome come Phiny</h2>
+      <h2 className='text-black-2'>What is next</h2>
+
+      </div>
   )
 }
 
