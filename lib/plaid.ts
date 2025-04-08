2a214409513cfa923e9dea1767e90fd0c877d3c3
ZEALOT <phinehasdscience@gmail.com>
2025-04-08 23:56:01 +0000

    initiate plaid api

---
+++ lib/plaid.ts
@@ -0,0 +1,14 @@
+
+import {Configuration,PlaidApi,PlaidEnvironments} from 'plaid'
+
+const configuration = new Configuration({
+    basePath :PlaidEnvironments.sanbox,
+    baseOptions:{
+        headers:{
+            "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
+            "PLAID_SECRET": process.env.PLAID_SECRET
+        }
+    }
+})
+
+export const plaidClient  = new PlaidApi(configuration);
